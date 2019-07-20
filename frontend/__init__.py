b0ebfed480c099d5add10111004407d0e07f0b1f
urban <urban@urban.pl>
2019-07-20 15:29:55 +0200

    frontend: basic trade list implemented

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -10,3 +10,8 @@ def home():
     return  render_template('home.html',
                             data={'given_name': 'currently in home'})
 
+@frontend_app.route('/trade_list')
+def list_trades():
+    trades = {'13413134': {'typology': 'spot', 'price': 400},
+              '92314124': {'typology': 'outright', 'price': 3000}}
+    return render_template('list_trades.html', trades=trades)
