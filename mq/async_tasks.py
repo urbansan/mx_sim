707920ed43d0a6880aa0d4af6c810550c8c63330
urban <urban@urban.pl>
2019-07-21 18:35:30 +0200

    celery subworkers are running pricing and communicate it by redis rooms

--- mq/async_tasks.py
+++ mq/async_tasks.py
@@ -1,27 +1,36 @@
 from celery import Celery
 from .. import connectivity
-
+import json
+import random
+import time
 celery_async = Celery('async_tasks', backend='redis://localhost', broker='redis://localhost')
 
+
 @celery_async.task
-def add(x, y):
-    print('workek started')
-    return x + y
+def send_single_price(trade, pricing_id):
+    publish = connectivity.get_pricing_room_publish_function(pricing_id)
+    price_update = {'trade_no': trade, 'price': random.randint(100, 1000)}
+    # time.sleep(random.random() * 1.5)
+    publish(message=json.dumps(price_update))
+    # return price_update
+
 
 @celery_async.task
 def price_by_pricing_id(pricing_id):
     print(f'pricing id: {pricing_id}')
-    import random
-    import time
-    import json
     trades = connectivity.get_trades_by_pricing_id(pricing_id)
-    random.shuffle(trades)
     publish = connectivity.get_pricing_room_publish_function(pricing_id)
-    for trade in trades:
-        time.sleep(0.2)
-        print(trade)
-        price_update = {'trade_no': trade, 'price': random.randint(100, 1000)}
-        publish(message=json.dumps(price_update))
+    random.shuffle(trades)
+    time.sleep(2)
+    tasks = []
+    price_count = 5000
+    for _ in range(price_count):
+        for trade in trades:
+            result = send_single_price.delay(trade, pricing_id)
+            tasks.append(result)
 
-    publish(message='FINISHED')
+
+    publish(message=str(len(trades) * price_count))
+
+
 
--- pricing/async_server.py
+++ pricing/async_server.py
@@ -5,6 +5,8 @@ import json
 import os
 from dotenv import load_dotenv
 from .. import connectivity
+from datetime import datetime as dt
+from concurrent.futures import ThreadPoolExecutor
 
 
 dirname = os.path.dirname(__file__)
@@ -15,23 +17,33 @@ load_dotenv(dotenv_path=dotenv_path)
 async def new_connection(ws, path):
     print('>> log >>', ws, path)
     pricing_id = path.strip('/')
-
-
+    start = dt.now()
     # while True:
     room_listener = connectivity.get_pricing_room_listener(pricing_id)
-    for message in room_listener.listen():
-        if message['type'] == 'message':
-            print(message)
-            if message['data'] == 'FINISHED':
-                # await ws.close()
-                break
-                # raise websockets.exceptions.ConnectionClosedOK(1001, 'pricing finished')
+    countdown = 0
+    countup = float('inf')
+    while True:
+        message = room_listener.get_message()
+        # print(message)
+        if message and message['type'] == 'message':
+
+            try:
+                countup = int(message['data'])
+            except (AttributeError, ValueError):
+                countdown += 1
 
             try:
                 await ws.send(message['data'])
             except websockets.exceptions.ConnectionClosedOK:
                 pass  #TODO: possible cleanup
-    print('end y\'all')
+        else:
+
+            print(f'countup: {countup}, countdown: {countdown}')
+            if countup - countdown < 1:
+                break
+            await asyncio.sleep(0.3)
+
+    print(f'end y\'all: {dt.now() - start}')
 
 
 def run_server():
